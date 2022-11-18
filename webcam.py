405b59675d5a13e4b229a57f4d64cd4bb3ba5796
Медведев Максим Александрович <medvedevma@rambler.ru>
2022-11-18 19:44:36 +0500

    Added array with pictures BGR formate

--- webcam.py
+++ webcam.py
@@ -2,6 +2,7 @@ import cv2
 cam = cv2.VideoCapture(0)
 cv2.namedWindow("Cam Example")
 img_counter = 0
+arr = []
 
 while True:
     ret, frame = cam.read()
@@ -17,8 +18,10 @@ while True:
         break
     elif k%256 == 32:
         img_name = "frame_{}.jpeg".format(img_counter)
+        arr.append(frame)
         cv2.imwrite(img_name, frame, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
         print("Screenshot taken")
+        cv2.imshow("Picture_{}.".format(img_counter), arr[img_counter])
         img_counter += 1
 
 cam.release()
